8d88e25c3e1ecd70f9c626ff6748da2f8569799a
Choi seoyun <yen731013@naver.com>
2023-12-13 13:42:45 +0900

    third commit

--- Term project.py
+++ Term project.py
@@ -1,3 +1,4 @@
+# 1. 홍승로
 import cv2
 import sys
 import mediapipe as mp
@@ -16,3 +17,24 @@ mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 hands = mp_hands.Hands(max_num_hands=5)
+# 2. 최서윤
+# 카메라 영상 가져오기
+cap = cv2.VideoCapture(0)
+
+# 카메라가 열렸는지 확인
+if not cap.isOpened():
+    print("Error: Camera is not opened")
+    sys.exit(1)
+
+frame_number = 0
+while True:
+    res, frame = cap.read()
+
+    if not res:
+        print("Error: camera error")
+        break
+
+    # 그레이스케일로 변환하여 얼굴 검출
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    
